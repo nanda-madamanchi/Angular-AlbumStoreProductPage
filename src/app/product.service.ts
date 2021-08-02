6e1eeaa1d1e943b245ff9ce17c83e09c79f1dec7
nanda-madamanchi <88310295+nanda-madamanchi@users.noreply.github.com>
2021-08-02 18:20:56 +0530

    serivce created

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,12 +6,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  constructor(private http: Http) { }
+  constructor(private _http: Http) { }
 
-  _albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   getAlbum(id:number){
-    return this.http.get(this._albumUrl).map(response => response.json())
+    return this._http.get(this._albumUrl).map(response => response.json())
   }
 
 }
